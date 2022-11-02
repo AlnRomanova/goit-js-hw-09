d721f278bc1e321a7f046b0198d158286b70cb84
AlnRomanova <111745365+AlnRomanova@users.noreply.github.com>
2022-11-02 16:04:27 +0000

    Deploying to gh-pages from @ AlnRomanova/goit-js-hw-09@4da263c74e1190ec25f2a35c65fa178303f6c7cd 🚀

---
+++ 01-color-switcher.6ab2628f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.body,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=null;e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,a=setInterval((function(){return t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){n.disabled=!0,e.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.6ab2628f.js.map
