4da263c74e1190ec25f2a35c65fa178303f6c7cd
Alina Romanova <alinakhraban@gmail.com>
2022-11-02 18:04:02 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyElColor = document.body;
+const colorBtnStart = document.querySelector('[data-start]');
+const colorBtnStop = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+
+colorBtnStart.addEventListener("click", () => {
+  colorBtnStart.disabled = true;
+  colorBtnStop.disabled = false;
+  timerId = setInterval(() => {
+  return bodyElColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+colorBtnStop.addEventListener("click", () => {
+  colorBtnStop.disabled = true;
+  colorBtnStart.disabled = false;
+  clearInterval(timerId);
+
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
