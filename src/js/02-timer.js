e6fd395bd21f4ee88c4debceb98345a053bbf123
Alina Romanova <alinakhraban@gmail.com>
2022-11-04 10:54:49 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,9 +3,9 @@ import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
 const btnStartTimer = document.querySelector('[data-start]');
-// const timerEl = document.querySelector('.timer');
+const timerEl = document.querySelector('.timer');
 const timerInput = document.querySelector('#datetime-picker')
-// const TIMER_DEADLINE = new Date(2022, 10, 3);
+
 btnStartTimer.disabled = true;
 
 
@@ -29,73 +29,58 @@ const options = {
 flatpickr (timerInput, options);
 
 const timer = {
-  start()  {
-    const startTime = Date.now();
-    setInterval(() => {
-    const currentTime = Date.now();
-    const ms = currentTime - startTime;
+  intervalId: null,
+
+  start(rootSelector, deadline) {
+
+    this.intervalId = setInterval(() => {
+     const now = new Date();
+     const ms = deadline - now;
 
      if (ms <= 0) {
-      stop();
+      this.stop();
       return;
-     };
+     }
 
-    const { days, hours, minutes, seconds } = convertMs(ms)
-   console.log(`${days}::${hours}::${minutes}::${seconds}`)
+     const {days, hours, minutes, seconds} = this.convertMs(ms);
 
-    }, 1000);
+     console.log({days, hours, minutes, seconds})
 
+     rootSelector.querySelector('[data-days]').textContent = this.addLeadingZero(days);
+     rootSelector.querySelector('[data-hours]').textContent = this.addLeadingZero(hours);
+     rootSelector.querySelector('[data-minutes]').textContent = this.addLeadingZero(minutes);
+     rootSelector.querySelector('[data-seconds]').textContent = this.addLeadingZero(seconds);
+
+    }, 1000)
+  },
+
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  },
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, 0);
   },
 
 };
 
-function addLeadingZero(value) {
-  return String(value).padStart(2, 0);
-};
-
-
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = addLeadingZero(Math.floor(ms / day));
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-
-  return { days, hours, minutes, seconds };
-}
-
 
 btnStartTimer.addEventListener('click', (e) => {
-  timer.start();
+  timer.start(timerEl, new Date(timerInput.value));
   });
 
 
-  // start(rootSelector, deadline) {
 
-  //   this.intervalId = setInterval(() => {
-  //    const now = new Date();
-  //    const ms = deadline - now;
-
-  //    if (ms <= 0) {
-  //     this.stop();
-  //     return;
-  //    }
-
-  //    const {days, hours, minutes, seconds} = this.convertMs(ms);
-
-  //    console.log({days, hours, minutes, seconds})
-
-  //    rootSelector.querySelector('[data-days]').textContent = this.addLeadingZero(days);
-  //    rootSelector.querySelector('[data-hours]').textContent = this.addLeadingZero(hours);
-  //    rootSelector.querySelector('[data-minutes]').textContent = this.addLeadingZero(minutes);
-  //    rootSelector.querySelector('[data-seconds]').textContent = this.addLeadingZero(seconds);
-
-  //   }, 1000)
-  // },
 
 
 
