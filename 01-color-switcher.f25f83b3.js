1154a46771cb47a85b0d6d387b5afae103d1ebe0
AlnRomanova <111745365+AlnRomanova@users.noreply.github.com>
2022-11-04 09:55:08 +0000

    Deploying to gh-pages from @ AlnRomanova/goit-js-hw-09@9d4d9a604ac21844bbbbd63d7a61af55a4a6fdbf 🚀

---
+++ 01-color-switcher.f25f83b3.js
@@ -0,0 +1,2 @@
+const e=document.body,t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.disabled=!0;let a=null;t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,a=setInterval((()=>e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3)})),d.addEventListener("click",(()=>{d.disabled=!0,t.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.f25f83b3.js.map
